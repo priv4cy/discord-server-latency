029413ca8619951ec41b5ec01eb20e5e0d9e7f16
Privacy <121909342+priv4cy@users.noreply.github.com>
2023-01-09 01:30:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client } = require('discord.js');
+const ALL_INTENTS = 32767;
+const bot = new Client({ intents: ALL_INTENTS, partials: ['MESSAGE', 'CHANNEL', 'REACTION'], });
+const token = 'token';
+
+bot.on('ready', () => {
+    console.log('Ready');
+    bot.application.commands.set([
+        { name: 'ping', description: 'Comprobar la latencia y el estado del Main Server', options: [], }
+    ]);
+});
+
+bot.on('interactionCreate', (interaction) => {
+    if (interaction.isCommand() && interaction.commandName === 'ping') {
+        const startTime = interaction.createdTimestamp;
+        interaction.channel.send('[Server Status] Main Server Online').then(sentMessage => {
+            const endTime = sentMessage.createdTimestamp;
+            const latency = endTime - startTime;
+            interaction.channel.send(`[Server Status] Main Server ${latency}ms`);
+        });
+    }
+});
+
+
+
+bot.login(token);
